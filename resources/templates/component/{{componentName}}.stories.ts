ac859c94b08b3ba05ea3d29c71b7871a48c0407b
Max <max.vandelaar@me.com>
2021-11-18 11:39:43 +0100

    Update and rename {{componentName}}.stories.js to {{componentName}}.stories.ts
    Updated storybook template

--- resources/templates/component/{{componentName}}.stories.ts
+++ resources/templates/component/{{componentName}}.stories.ts
@@ -1,4 +1,7 @@
 import {{ componentNamePascalCase }} from './{{componentName}}.vue';
+import { Story } from '@storybook/vue3'
+import { constructArgType } from '../../../storybook/utils/utils'
+
 {{# createMarkdown = "1" }}
 import markdown from './{{componentName}}.md';
 {{/ createMarkdown = "1"}}
@@ -6,15 +9,30 @@ import markdown from './{{componentName}}.md';
 export default {
     title: '{{ componentNamePascalCase }}',
     component: {{ componentNamePascalCase }},
+    argTypes: {
+    },
     {{# createMarkdown = "1" }}
     parameters: {
         notes: { markdown }
     },
     {{/ createMarkdown = "1" }}
 };
-
-export const Default = (args, {argTypes}) => ({
-    props: Object.keys(argTypes),
+     
+type Args = {
+}
+     
+const Template: Story<Args> = args => ({
     components: { {{ componentNamePascalCase }} },
-    template: `<{{ componentName }} v-bind="$props" />`,
-});
+    setup () {
+        return {
+            args,
+        }
+    }
+    template: '<{{ componentName }} v-bind="$props" />',
+})
+
+export const Default = Template.bind({})
+Default.args = {}
+
+ 
+ 
